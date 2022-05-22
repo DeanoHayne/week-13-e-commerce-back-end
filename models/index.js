95e241cba7fcbc3828980829cf6f13e331d75343
Dean Hay <deanhay16@gmail.com>
2022-05-22 20:52:19 +1000

    completed model index

--- models/index.js
+++ models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
+Category.hasMany(Category, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  model: 'product_tag'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  model: 'product_tag'
+});
 
 module.exports = {
   Product,
