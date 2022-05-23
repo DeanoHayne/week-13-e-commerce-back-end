47b1e355a20846965328ce861a58b176297df9d7
Dean Hay <deanhay16@gmail.com>
2022-05-23 17:27:57 +1000

    updated product-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -17,6 +17,7 @@ router.get('/', (req, res) => {
 
 
 router.get('/:id', (req, res) => {
+
   // find one category by its `id` value
   // be sure to include its associated Products
 
@@ -38,6 +39,7 @@ router.get('/:id', (req, res) => {
 
 
 router.post('/', (req, res) => {
+
   // create a new category
 
   try {
@@ -49,6 +51,7 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
+
   // update a category by its `id` value
 
   try {
@@ -68,7 +71,9 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
+
   // delete a category by its `id` value
+
   try {
     const categoryData = await Category.destroy({
       where: {
