cd44c758c995b916c1612a13d845219a8a996272
Dean Hay <deanhay16@gmail.com>
2022-05-24 16:22:52 +1000

    updating changes to file

--- models/index.js
+++ models/index.js
@@ -5,21 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
+
 // Categories have many Products
-Category.hasMany(Category, {
+
+Category.hasMany(Product, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  model: 'product_tag'
+  through: {
+    model: 'product_tag'
+
+  },
 });
+
+
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  model: 'product_tag'
+  through: {
+    model: 'product_tag'
+  }
 });
 
 module.exports = {
